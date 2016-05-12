ca28cb321e2d54259cf1dd692acba76836413178
Rayest <yi@rayest.cn>
2016-05-12 23:05:34 +0800

    springmvc with user input and save

---
+++ src/main/java/cn/rayest/controller/InputProductController.java
@@ -0,0 +1,22 @@
+package cn.rayest.controller;
+
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by Administrator on 2016/5/12 0012.
+ */
+public class InputProductController implements Controller {
+    private static final Log logger = LogFactory.getLog(InputProductController.class);
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception{
+        logger.info("InputProductController called");
+        return new ModelAndView("/WEB-INF/jsp/ProductForm.jsp");
+    }
+}
